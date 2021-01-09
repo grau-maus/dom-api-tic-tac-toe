d555886b5787a5b2461c310abf929d6d19bcfdba
josh <joshtupas@gmail.com>
2021-01-09 13:23:04 -0600

    Refactored code to make it EASIER TO FOLLOW. AI / nightmare mode will be implemented soon. Also, helpful comment descriptions will need to be added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,224 +1,193 @@
-let squareValues = Array(9);
-let gameStatus = '';
-let announcementStatus = false;
-let nightmareMode = false;
-let nextPlayer = false;
-let aiPlayer = '';
+//DOM API-nnnnnnnnnnnnnnnnnn-
+let xIMG;
+let oIMG;
+let divGame;
+let h1GameStatus;
+let divGridArray;
+let btnNewGame;
+let btnGiveUp;
+//DOM API-uuuuuuuuuuuuuuuuuu-
 
-function checkGameStatus() {
-    for (let j = 0; j < squareValues.length; j += 3) {
-        if (squareValues[j] !== undefined && squareValues[j] === squareValues[j + 1] && squareValues[j] === squareValues[j + 2]) {
-            gameStatus = squareValues[j];
-            break;
-        }
-    }
+//BOOLEAN-nnnnnnnnnnnnnnnnnn-
+let secondPlayer;
+let aiStatusOn;
+//BOOLEAN-uuuuuuuuuuuuuuuuuu-
 
-    for (let i = 0; i < squareValues.length; i++) {
-        if (squareValues[i] !== undefined && squareValues[i] === squareValues[i + 3] && squareValues[i] === squareValues[i + 6]) {
-            gameStatus = squareValues[i];
-            break;
-        }
-    }
+//ARRAY-nnnnnnnnnnnnnnnnnnnn-
+let squareArray;
+//ARRAY-uuuuuuuuuuuuuuuuuuuu-
 
-    if (squareValues[0] !== '' && squareValues[0] === squareValues[4] && squareValues[0] === squareValues[8]) {
-        gameStatus = squareValues[0];
+//IMMUTABLE-nnnnnnnnnnnnnnnn-
+let aiXO;
+//IMMUTABLE-uuuuuuuuuuuuuuuu-
 
-    } else if (squareValues[2] !== '' && squareValues[2] === squareValues[4] && squareValues[2] === squareValues[6]) {
-        gameStatus = squareValues[2];
-    }
+function newGame() {
+    //DOM API-nnnnnnnnnnnnnnnnnn-
+    xIMG = '<img src="./xo/player-x.svg">';
+    oIMG = '<img src="./xo/player-o.svg">';
+    divGame = document.querySelector("#game");
+    divGridArray = document.querySelectorAll(".square");
+    h1GameStatus = document.querySelector("#game-status");
+    btnNewGame = document.querySelectorAll("button")[0];
+    btnGiveUp = document.querySelectorAll("button")[1];
+    btnNewGame.setAttribute("id", "new-game");
+    btnGiveUp.setAttribute("id", "give-up");
+    //DOM API-uuuuuuuuuuuuuuuuuu-
 
-    if (gameStatus) {
-        announcementStatus = true;
-        document.getElementById("game-status").innerText = `Winner: ${gameStatus}`;
+    //BOOLEAN-nnnnnnnnnnnnnnnnnn-
+    secondPlayer = false;
+    aiStatusOn = false;
+    //BOOLEAN-uuuuuuuuuuuuuuuuuu-
 
-    } else {
-        for (let i = 0; i < squareValues.length; i++) {
-            if (!squareValues[i]) {
-                return;
-            }
-        }
-        announcementStatus = true;
-        document.getElementById("game-status").innerText = `Tie!`;
+    //ARRAY-nnnnnnnnnnnnnnnnnnnn-
+    squareArray = Array(9);
+    //ARRAY-uuuuuuuuuuuuuuuuuuuu-
+
+    h1GameStatus.innerText = null;
+    btnNewGame.disabled = true;
+    btnGiveUp.disabled = false;
+    localStorage.clear();
+
+    for (let i = 0; i < divGridArray.length; i++) {
+        divGridArray[i].innerHTML = null;
     }
 }
 
-function aiMode() {
-    if (nightmareMode && document.getElementById("give-up").disabled) {
-        let min = Math.ceil(0);
-        let max = Math.floor(1);
-        let aiSymbol = Math.floor(Math.random() * (max - min + 1) + min);
+function parseInput(event) {
+    if (aiStatusOn && aiXO === 'X' && !h1GameStatus.innerText) {
+        aiMove();
+        gameUpdate();
+    }
 
-        if (aiSymbol === 0) {
-            aiPlayer = 'X';
+    if (event.target.id.startsWith("square") && !event.target.innerHTML && !h1GameStatus.innerText) {
+        const squareValue = Number.parseInt(event.target.id[event.target.id.length - 1]);
+
+        if (!secondPlayer) {
+            event.target.innerHTML = xIMG;
+
         } else {
-            aiPlayer = 'O';
+            event.target.innerHTML = oIMG;
         }
+        gameUpdate(squareValue);
 
-        if (aiPlayer === 'X') {
-            let xo = document.createElement("img");
-            let min = Math.ceil(0);
-            let max = Math.floor(8);
-            let randomIndex = Math.floor(Math.random() * (max - min + 1) + min);
-            let firstMove = document.getElementsByClassName("square");
+    } else if (event.target.id === "new-game") {
+        newGame();
 
-            nextPlayer = true;
-            xo.setAttribute("src", "player-x.svg");
-            firstMove[randomIndex].appendChild(xo);
-            squareValues[randomIndex] = 'X';
-        }
+    } else if (event.target.id === "give-up") {
+        giveUp();
+    }
+
+    if (aiStatusOn && aiXO === 'O' && !h1GameStatus.innerText) {
+        aiMove();
+        gameUpdate();
     }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-    let boardState = localStorage.getItem("boardState");
-    let lastClicked = localStorage.getItem("latestSquare");
-    let aiStatus = localStorage.getItem("aiStatus");
-    let grid = document.getElementById("tic-tac-toe-board");
-    let gridSquares = document.getElementsByClassName("square");
-    let newGame = document.querySelectorAll("button")[0];
-    let announcement = document.getElementById("game-status");
-    let giveUp = document.querySelectorAll("button")[1];
+function drawXO(gridValue) {
+    if (!secondPlayer) {
+        squareArray[gridValue] = 'X';
+    } else {
+        squareArray[gridValue] = 'O';
+    }
+}
 
-    giveUp.setAttribute("id", "give-up");
-    newGame.setAttribute("id", "new-game");
-    newGame.disabled = true;
-    giveUp.disabled = true;
+function announceWinner(symbol) {
+    if (symbol === "Tie!") {
+        h1GameStatus.innerText = symbol;
+    } else {
+        h1GameStatus.innerText = `Winner: ${symbol}`
+    }
 
-    if (boardState || aiStatus) {
-        giveUp.disabled = false;
-        squareValues = JSON.parse(boardState);
-        nightmareMode = JSON.parse(aiStatus);
-        lastClicked = Number.parseInt(lastClicked);
+    btnNewGame.disabled = false;
+    btnGiveUp.disabled = true;
+}
 
-        for (let i = 0; i < squareValues.length; i++) {
-            if (squareValues[i]) {
-                let xo = document.createElement("img");
+function gameUpdate(playerValue) {
+    if (playerValue) {
+        drawXO(playerValue);
+    }
 
-                if (squareValues[i] === 'X') {
-                    xo.setAttribute("src", "./player-x.svg");
-
-                } else if (squareValues[i] === 'O') {
-                    xo.setAttribute("src", "./player-o.svg");
-                }
-
-                gridSquares[i].appendChild(xo);
-            }
-        }
-
-        if (squareValues[lastClicked] === 'X') {
-            nextPlayer = true;
+    for (let i = 0; i < squareArray.length; i += 3) {
+        if (squareArray[i] !== undefined && squareArray[i] === squareArray[i + 1] && squareArray[i] === squareArray[i + 2]) {
+            announceWinner(squareArray[i]);
+            break;
         }
     }
 
-    aiMode();
+    for (let i = 0; i < squareArray.length; i++) {
+        if (squareArray[i] !== undefined && squareArray[i] === squareArray[i + 3] && squareArray[i] === squareArray[i + 6]) {
+            announceWinner(squareArray[i]);
+            break;
+        }
+    }
 
-    grid.addEventListener("click", event => {
-        if (event.target.id !== event.currentTarget.id && !announcementStatus && event.target.id.startsWith("square")) {
-            let curSquare = event.target;
-            let boxNum = event.target.id;
+    if (squareArray[0] !== undefined && squareArray[0] === squareArray[4] && squareArray[0] === squareArray[8]) {
+        announceWinner(squareArray[0]);
 
-            giveUp.disabled = false;
-            boxNum = boxNum[boxNum.length - 1];
-            boxNum = Number.parseInt(boxNum);
+    } else if (squareArray[2] !== undefined && squareArray[2] === squareArray[4] && squareArray[2] === squareArray[6]) {
+        announceWinner(squareArray[2]);
+    }
 
-            if (nightmareMode && aiPlayer === 'X') {
-                checkGameStatus();
-            }
+    if (h1GameStatus.innerText) {
+        return;
+    } else {
+        let filledSquares = 1;
 
-            if (!squareValues[boxNum] && !announcementStatus) {
-                let xo = document.createElement("img");
-
-                if (!nextPlayer) {
-                    xo.setAttribute("src", "player-x.svg");
-                    curSquare.appendChild(xo);
-                    squareValues[boxNum] = 'X';
-
-                } else if (nightmareMode && nextPlayer || !nightmareMode && nextPlayer) {
-                    xo.setAttribute("src", "player-o.svg");
-                    curSquare.appendChild(xo);
-                    squareValues[boxNum] = 'O';
-                }
-                nextPlayer = !nextPlayer;
-
-                checkGameStatus();
-
-                if (nightmareMode && !announcementStatus) {
-                    let xo = document.createElement("img");
-
-                    while (curSquare.innerHTML) {
-                        let min = Math.ceil(0);
-                        let max = Math.floor(8);
-                        let randomIndex = Math.floor(Math.random() * (max - min + 1) + min);
-
-                        if (!gridSquares[randomIndex].innerHTML) {
-                            curSquare = gridSquares[randomIndex];
-                            boxNum = randomIndex;
-                        }
-                    }
-
-                    if (aiPlayer === 'X' && !nextPlayer) {
-                        xo.setAttribute("src", "player-x.svg");
-                        curSquare.appendChild(xo);
-                        squareValues[boxNum] = 'X';
-
-                    } else if (aiPlayer === 'O') {
-                        xo.setAttribute("src", "player-o.svg");
-                        curSquare.appendChild(xo);
-                        squareValues[boxNum] = 'O';
-                    }
-                    nextPlayer = !nextPlayer;
-                }
-                localStorage.setItem("latestSquare", boxNum);
+        for (let i = 0; i < squareArray.length; i++) {
+            if (squareArray[i]) {
+                filledSquares++;
             }
         }
-        if (announcementStatus) {
-            newGame.disabled = false;
-            giveUp.disabled = true;
-            nightmareMode = false;
-        }
 
-        localStorage.setItem("boardState", JSON.stringify(squareValues));
-        // localStorage.setItem();
+        if (filledSquares === squareArray.length) {
+            announceWinner("Tie!");
+            gameUpdate();
+        }
+    }
+
+    secondPlayer = !secondPlayer;
+}
+
+function giveUp() {
+    if (secondPlayer) {
+        announceWinner("X");
+    } else {
+        announceWinner("O");
+    }
+
+    btnNewGame.disabled = false;
+    btnGiveUp.disabled = true;
+}
+
+function aiMove() {
+
+}
+
+function saveState() {
+    localStorage.setItem("squareArray", JSON.stringify(squareArray));
+    localStorage.setItem("secondPlayer", JSON.stringify(secondPlayer));
+    localStorage.setItem("h1GameStatus", JSON.stringify(h1GameStatus));
+    localStorage.setItem("aiStatusOn", JSON.stringify(aiStatusOn));
+    localStorage.setItem("aiXO", JSON.stringify(aiXO));
+}
+
+function loadState() {
+    squareArray = JSON.parse(localStorage.getItem("squareArray"));
+    secondPlayer = JSON.parse(localStorage.getItem("secondPlayer"));
+    h1GameStatus = JSON.parse(localStorage.getItem("h1GameStatus"));
+    aiStatusOn = JSON.parse(localStorage.getItem("aiStatusOn"));
+    aiXO = JSON.parse(localStorage.getItem("aiXO"));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("secondPlayer")) {
+        loadState();
+    } else {
+        newGame();
+    }
+
+    document.addEventListener("click", event => {
+        parseInput(event);
+        saveState();
     });
-
-    document.getElementsByClassName("actions")[0].addEventListener("click", event => {
-        if (event.target.id === "new-game") {
-            newGame.disabled = true;
-            nextPlayer = false;
-            announcementStatus = false;
-            announcement.innerText = null;
-            squareValues = Array(9);
-            gameStatus = '';
-
-            localStorage.clear();
-            localStorage.setItem("aiStatus", "false");
-
-            for (let i = 0; i < gridSquares.length; i++) {
-                gridSquares[i].innerHTML = null;
-            }
-
-            let min = Math.ceil(0);
-            let max = Math.floor(1);
-            let aiChance = Math.floor(Math.random() * (max - min + 1) + min);
-            let compare = Math.floor(Math.random() * (max - min + 1) + min);
-
-            if (aiChance === compare) {
-                nightmareMode = true;
-                localStorage.setItem("aiStatus", "true");
-            }
-            aiMode();
-
-        } else if (event.target.id === "give-up") {
-            newGame.disabled = false;
-            giveUp.disabled = true;
-            announcementStatus = true;
-
-            if (nextPlayer) {
-                announcement.innerText = "Winner: X";
-
-            } else {
-                announcement.innerText = "Winner: O";
-            }
-        }
-    });
-})
+});
